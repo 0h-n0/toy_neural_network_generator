59bb962688dbbf122a8b03f10c6e99420869139c
0h-n0 <kbu94982@gmail.com>
2020-03-05 19:45:45 +0900

    fix: multimodal bugs.

--- examples/pytorch_multimodal_example.py
+++ examples/pytorch_multimodal_example.py
@@ -1,56 +1,3 @@
-![Github CI/CD](https://github.com/0h-n0/toy_neural_network_generator/workflows/Github%20CI/CD/badge.svg?branch=master)
-
-# Toy Neural Network Generator
-
-
-## Simple Model Generator
-
-```python
-#!/usr/bin/env python
-import torch
-import torch.nn as nn
-import torchex.nn as exnn
-from tnng import Generator, MultiHeadLinkedListLayer
-
-m = MultiHeadLinkedListLayer()
-# all layers can be lazy evaluation.
-m.append([exnn.Linear(64), exnn.Linear(128), exnn.Linear(256)])
-m.append([nn.ReLU(), nn.ELU()])
-m.append([exnn.Linear(16), exnn.Linear(32), exnn.Linear(64),])
-m.append([nn.ReLU(), nn.ELU()])
-m.append([exnn.Linear(10)])
-
-g = Generator(m)
-
-x = torch.randn(128, 256)
-
-class Model(nn.Module):
-    def __init__(self, idx=0):
-        super(Model, self).__init__()
-        self.model = nn.ModuleList([l[0] for l in g[idx]])
-
-    def forward(self, x):
-        for m in self.model:
-            x = m(x)
-        return x
-
-m = Model(0)
-o = m(x)
-
-'''
-ModuleList(
-  (0): Linear(in_features=256, out_features=64, bias=True)
-  (1): ReLU()
-  (2): Linear(in_features=64, out_features=16, bias=True)
-  (3): ReLU()
-  (4): Linear(in_features=16, out_features=10, bias=True)
-)
-'''
-```
-
-## Multimodal Model Generator
-
-```
 #!/usr/bin/env python
 import torch
 import torch.nn as nn
@@ -104,4 +51,3 @@ img = torch.randn(128, 3, 28, 28)
 m = Model()
 o = m(x, img)
 print(o.shape)
-```
