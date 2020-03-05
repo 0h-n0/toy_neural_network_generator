f190af005e73c5d74e63a9635ce96a8cb3e8006e
0h-n0 <kbu94982@gmail.com>
2020-03-05 15:36:54 +0900

    feat: get layer index list method .

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,5 +1,6 @@
-from tnng import MultiHeadLinkedListLayer, Generator
+import pytest
 
+from tnng import MultiHeadLinkedListLayer, Generator
 
 def test_generator_length():
     m = MultiHeadLinkedListLayer()
@@ -9,8 +10,7 @@ def test_generator_length():
     g = Generator(m)
     assert len(g) == 15
 
-
-def test_multi_modal_generator_length_():
+def test_multi_modal_generator_length():
     m1 = MultiHeadLinkedListLayer()
     m1.append([1, 2, 3, 4, 5])
     m1.append([6, 7, 8])
@@ -22,3 +22,44 @@ def test_multi_modal_generator_length_():
     m.append([100, 200])
     g = Generator(m)
     assert len(g) == 450
+
+@pytest.fixture(
+    params=[
+        [0, [[0], [0], [0]]],
+        [1, [[0], [1], [0]]],
+        [2, [[0], [2], [0]]],
+        [3, [[0], [0], [1]]],
+        [4, [[0], [1], [1]]],
+        [5, [[0], [2], [1]]],
+        [14, [[0], [2], [4]]],
+        [-1, [[0], [2], [4]]],
+    ]
+)
+def index_acccess_test_case(request):
+    input = request.param[0]
+    expected = request.param[1]
+    return input, expected
+
+def test_get_layer_index_list(index_acccess_test_case):
+    input = index_acccess_test_case[0]
+    expected = index_acccess_test_case[1]
+    m = MultiHeadLinkedListLayer()
+    m.append([1, 2, 3, 4, 5])
+    m.append([6, 7, 8])
+    m.append([10])
+    g = Generator(m)
+    assert g._get_layer_index_list(input) == expected
+
+
+def test_layer_num_list_multimodal():
+    m = MultiHeadLinkedListLayer()
+    m.append([1, 2, 3, 4, 5])
+    m.append([6, 7, 8])
+    m.append([10])
+    m1 = MultiHeadLinkedListLayer()
+    m1.append([10, 20, 30, 40, 50])
+    m1.append([60, 70, 80])
+    m = m + m1
+    m.append([100, 200])
+    g = Generator(m)
+    assert [1, 6, 10] == g[1]
--- tnng/generator.py
+++ tnng/generator.py
@@ -4,14 +4,16 @@ from .layer import MultiHeadLinkedListLayer
 class Generator:
     def __init__(self, multi_head_linked_list_layer: MultiHeadLinkedListLayer):
         self.multi_head_linked_list_layer = multi_head_linked_list_layer
-        self._len = self._preprocess(self.multi_head_linked_list_layer)
+        self._len, self.layer_num_list = self._preprocess(self.multi_head_linked_list_layer)
 
     def _preprocess(self, multi_head_linked_list_layer) -> int:
         cur = [multi_head_linked_list_layer.tail,]
         num = 1
         num *= len(multi_head_linked_list_layer.tail.layers)
-        for _ in range(multi_head_linked_list_layer.depth):
+        num_parents = [[len(multi_head_linked_list_layer.tail.layers),],]
+        for _ in range(multi_head_linked_list_layer.depth-1):
             parents = []
+            _each_layer_num_parents = []
             for c in cur:
                 if c is None or c.parent is None:
                     parents.append(None)
@@ -20,12 +22,27 @@ class Generator:
                     parents.append(p)
                     if p.layers is not None:
                         num *= len(p.layers)
-                print(num)
+                        _each_layer_num_parents.append(len(p.layers))
+                    else:
+                        _each_layer_num_parents.append(1)
+            if _each_layer_num_parents:
+                num_parents.append(_each_layer_num_parents)
             cur = parents
-        return num
+        return num, num_parents
 
     def __getitem__(self, idx):
-        pass
+        layer_index_list = self._get_layer_index_list(idx)
+
+    def _get_layer_index_list(self, idx):
+        _idx = idx
+        layer_index_list = []
+        for eles in self.layer_num_list:
+            _index_list = []
+            for i, ele in enumerate(eles):
+                _index_list.append(_idx % ele)
+                _idx //= ele
+            layer_index_list.append(_index_list)
+        return layer_index_list
 
     def __len__(self):
         return self._len
