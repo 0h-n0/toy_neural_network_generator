024c8564c47b4f5d9338eb30df91248ae285f064
0h-n0 <kbu94982@gmail.com>
2020-03-15 13:47:30 +0900

    feat: generator can return layers.

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -21,136 +21,6 @@ except:
     exist_torch = False
 
 
-def test_generator_length():
-    m = MultiHeadLinkedListLayer()
-    m.append([1, 2, 3, 4, 5])
-    m.append([6, 7, 8])
-    m.append([10])
-    g = Generator(m)
-    assert len(g) == 15
-
-def test_multi_modal_generator_length():
-    m1 = MultiHeadLinkedListLayer()
-    m1.append([1, 2, 3, 4, 5])
-    m1.append([6, 7, 8])
-    m1.append([10])
-    m2 = MultiHeadLinkedListLayer()
-    m1.append([10, 20, 30, 40, 50])
-    m1.append([60, 70, 80])
-    m = m1 + m2
-    m.append([100, 200])
-    g = Generator(m)
-    assert len(g) == 450
-
-@pytest.fixture(
-    params=[
-        [0, [[0], [0], [0]]],
-        [1, [[0], [1], [0]]],
-        [2, [[0], [2], [0]]],
-        [3, [[0], [0], [1]]],
-        [4, [[0], [1], [1]]],
-        [5, [[0], [2], [1]]],
-        [14, [[0], [2], [4]]],
-        [-1, [[0], [2], [4]]],
-    ]
-)
-def index_get_layer_index_list_test_case(request):
-    input = request.param[0]
-    expected = request.param[1]
-    return input, expected
-
-def test_get_layer_index_list(index_get_layer_index_list_test_case):
-    input = index_get_layer_index_list_test_case[0]
-    expected = index_get_layer_index_list_test_case[1]
-    m = MultiHeadLinkedListLayer()
-    m.append([1, 2, 3, 4, 5])
-    m.append([6, 7, 8])
-    m.append([10])
-    g = Generator(m)
-    assert g._get_layer_index_list(input) == expected
-
-
-@pytest.fixture(
-    params=[
-        [0, [[0], [0], [0, 0], [0, 0], [0, 0]]],
-        [1, [[1], [0], [0, 0], [0, 0], [0, 0]]],
-        [2, [[0], [0], [0, 1], [0, 0], [0, 0]]],
-        [3, [[1], [0], [0, 1], [0, 0], [0, 0]]],
-        [4, [[0], [0], [0, 2], [0, 0], [0, 0]]],
-        [5, [[1], [0], [0, 2], [0, 0], [0, 0]]],
-        [449, [[1], [0], [0, 2], [2, 4], [4, 0]]],
-        [-1, [[1], [0], [0, 2], [2, 4], [4, 0]]],
-        [-2, [[0], [0], [0, 2], [2, 4], [4, 0]]],
-    ]
-)
-def index_multimodal_get_layer_index_list_test_case(request):
-    input = request.param[0]
-    expected = request.param[1]
-    return input, expected
-
-def test_layer_num_list_multimodal(index_multimodal_get_layer_index_list_test_case):
-    input = index_multimodal_get_layer_index_list_test_case[0]
-    expected = index_multimodal_get_layer_index_list_test_case[1]
-    m = MultiHeadLinkedListLayer()
-    m.append([1, 2, 3, 4, 5])
-    m.append([6, 7, 8])
-    m.append([10])
-    m1 = MultiHeadLinkedListLayer()
-    m1.append([10, 20, 30, 40, 50])
-    m1.append([60, 70, 80])
-    m = m + m1
-    m.append([100, 200])
-    g = Generator(m)
-    assert g._get_layer_index_list(input) == expected
-
-@pytest.mark.xfail
-def test_layer_index_access_error():
-    m = MultiHeadLinkedListLayer()
-    m.append([1, 2, 3, 4, 5])
-    g = Generator(m)
-    g[10]
-
-
-def test_index_access():
-    m = MultiHeadLinkedListLayer()
-    m.append([1, 2, 3, 4, 5])
-    m.append([10, 20, 30, 40, 50])
-    m.append([100, 200])
-    m.append([1000, 2000, 3000, 4000])
-    m.append([10000, 20000, 30000, 40000])
-    g = Generator(m)
-    assert g[3] == [[1], [10], [100], [1000], [40000]]
-
-
-def test_multi_modal_index_access():
-    m = MultiHeadLinkedListLayer()
-    m.append([1, 2, 3, 4, 5])
-    m.append([10, 20, 30, 40, 50])
-    m1 = MultiHeadLinkedListLayer()
-    m1.append([100, 200])
-    m1.append([1000, 2000, 3000, 4000])
-    m1.append([10000, 20000, 30000, 40000])
-    m = m1 + m
-    m.append([100])
-    g = Generator(m)
-    assert g[3] == [[100, None], [1000, 1], [40000, 10], [DummyConcat], [100]]
-
-
-def test_multi_modal2_index_access():
-    m = MultiHeadLinkedListLayer()
-    m.append([1, 2, 3, 4, 5])
-    m.append([10, 20, 30, 40, 50])
-    m1 = MultiHeadLinkedListLayer()
-    m1.append([100, 200])
-    m1.append([1000, 2000, 3000, 4000])
-    m1.append([10000, 20000, 30000, 40000])
-    m1.append([100000, 200000, 300000, 400000])
-    m1.append([1000000, 2000000, 3000000, 4000000])
-    m = m1 + m
-    m.append([100])
-    g = Generator(m)
-    assert g[3] == [[100, None], [1000, None], [10000, None], [100000, 1], [4000000, 10], [DummyConcat], [100]]
-
 
 
 class Hoge1:
@@ -176,6 +46,188 @@ class Hoge4:
         self.units = units
     def __call__(self, x):
         pass
+
+
+def test_generator_length():
+    m = MultiHeadLinkedListLayer()
+    m.append([1, 2, 3, 4, 5])
+    m.append([6, 7, 8])
+    m.append([10])
+    g = Generator(m)
+    assert len(g) == 15
+
+def test_multi_modal_generator_length():
+    m1 = MultiHeadLinkedListLayer()
+    m1.append([1, 2, 3, 4, 5])
+    m1.append([6, 7, 8])
+    m1.append([10])
+    m2 = MultiHeadLinkedListLayer()
+    m1.append([10, 20, 30, 40, 50])
+    m1.append([60, 70, 80])
+    m = m1 + m2
+    m.append([100, 200])
+    g = Generator(m)
+    assert len(g) == 450
+
+@pytest.fixture(
+    params=[
+        [0, [0, 0, 0]],
+        [1, [0, 0, 1]],
+        [2, [0, 0, 2]],
+        [3, [0, 0, 3]],
+        [4, [0, 0, 4]],
+        [5, [0, 1, 0]],
+        [14, [0, 2, 4]],
+        [-1, [0, 2, 4]],
+    ]
+)
+def index_get_layer_index_list_test_case(request):
+    input = request.param[0]
+    expected = request.param[1]
+    return input, expected
+
+def test_get_layer_index_list(index_get_layer_index_list_test_case):
+    input = index_get_layer_index_list_test_case[0]
+    expected = index_get_layer_index_list_test_case[1]
+    m = MultiHeadLinkedListLayer()
+    m.append([1, 2, 3, 4, 5])
+    m.append([6, 7, 8])
+    m.append([10])
+    g = Generator(m)
+    assert g._get_layer_index_list(input, g._node_type_layers) == expected
+
+
+@pytest.fixture(
+    params=[
+        [0, [0, 0, 0, 0, 0, 0, 0]],
+        [1, [0, 0, 0, 0, 0, 0, 1]],
+        [2, [0, 0, 0, 0, 1, 0, 0]],
+        [3, [1, 0, 1, 0, 0, 0, 0]],
+        [4, [0, 0, 2, 0, 0, 0, 0]],
+        [5, [1, 0, 2, 0, 0, 0, 0]],
+        [449, [1, 0, 2, 2, 4, 4, 0]],
+        [-1, [1, 0, 2, 2, 4, 4, 0]],
+        [-2, [0, 0, 2, 2, 4, 4, 0]],
+    ]
+)
+def index_multimodal_get_layer_index_list_test_case(request):
+    input = request.param[0]
+    expected = request.param[1]
+    return input, expected
+
+def test_layer_num_list_multimodal(index_multimodal_get_layer_index_list_test_case):
+    input = index_multimodal_get_layer_index_list_test_case[0]
+    expected = index_multimodal_get_layer_index_list_test_case[1]
+    m = MultiHeadLinkedListLayer()
+    m.append([1, 2, 3, 4, 5])
+    m.append([6, 7, 8])
+    m.append([10])
+    m1 = MultiHeadLinkedListLayer()
+    m1.append([10, 20, 30, 40, 50])
+    m1.append([60, 70, 80])
+    m = m + m1
+    m.append([100, 200])
+    g = Generator(m)
+    assert len(g._node_type_layers) == 7
+    assert g._get_layer_index_list(input, g._node_type_layers) == expected
+
+@pytest.mark.xfail
+def test_layer_index_access_error():
+    m = MultiHeadLinkedListLayer()
+    m.append([1, 2, 3, 4, 5])
+    g = Generator(m)
+    g[10]
+
+
+def test_index_access():
+    m = MultiHeadLinkedListLayer()
+    m.append([1, 2, 3, 4, 5])
+    m.append([10, 20, 30, 40, 50])
+    m.append([100, 200])
+    m.append([1000, 2000, 3000, 4000])
+    m.append([10000, 20000, 30000, 40000])
+    g = Generator(m)
+    assert g[3] == [[4], [10], [100], [1000], [10000]]
+
+
+def test_append_lazy_index_access():
+    m = MultiHeadLinkedListLayer()
+    m.append_lazy(Hoge1, [{'units': i} for i in range(1, 6)])
+    m.append_lazy(Hoge1, [{'units': i} for i in range(10, 60, 10)])
+    m.append_lazy(Hoge1, [{'units': i} for i in range(100, 300, 1000)])
+    m.append_lazy(Hoge1, [{'units': i} for i in range(1000, 5000, 1000)])
+    m.append_lazy(Hoge1, [{'units': i} for i in range(10000, 50000, 100000)])
+    g = Generator(m)
+    g[3]
+
+
+def test_multi_modal_index_access():
+    m = MultiHeadLinkedListLayer()
+    m.append([1, 2, 3, 4, 5])
+    m.append([10, 20, 30, 40, 50])
+    m1 = MultiHeadLinkedListLayer()
+    m1.append([100, 200])
+    m1.append([1000, 2000, 3000, 4000])
+    m1.append([10000, 20000, 30000, 40000])
+    m = m1 + m
+    m.append([100])
+    g = Generator(m)
+    assert len(g[3]) == 5
+    assert g[3] == [[200, None], [2000, 1], [10000, 10], ['concat'], [100]]
+
+
+def test_multi_modal2_index_access():
+    m = MultiHeadLinkedListLayer()
+    m.append([1, 2, 3, 4, 5])
+    m.append([10, 20, 30, 40, 50])
+    m1 = MultiHeadLinkedListLayer()
+    m1.append([100, 200])
+    m1.append([1000, 2000, 3000, 4000])
+    m1.append([10000, 20000, 30000, 40000])
+    m1.append([100000, 200000, 300000, 400000])
+    m1.append([1000000, 2000000, 3000000, 4000000])
+    m = m1 + m
+    m.append([100])
+    g = Generator(m)
+    print(g[3])
+    assert g[3] == [[200, None], [2000, None], [10000, None], [100000, 1], [1000000, 10], ['concat'], [100]]
+
+
+def test_multi_modal2_index_access_2():
+    m = MultiHeadLinkedListLayer()
+    m.append([1, 2, 3, 4, 5])
+    m.append([10, 20, 30, 40, 50])
+    m1 = MultiHeadLinkedListLayer()
+    m1.append([100, 200])
+    m1.append([1000, 2000, 3000, 4000])
+    m1.append([10000, 20000, 30000, 40000])
+    m1.append([100000, 200000, 300000, 400000])
+    m1.append([1000000, 2000000, 3000000, 4000000])
+    m = m + m1
+    m.append([100])
+    g = Generator(m)
+    assert g[3] == [[100, None], [1000, None], [10000, None], [100000, 1], [4000000, 10], [DummyConcat], [100]]
+
+def test_multi_modal2_index_access_3():
+    m = MultiHeadLinkedListLayer()
+    m.append([1, 2, 3, 4, 5])
+    m.append([10, 20, 30, 40, 50])
+    m1 = MultiHeadLinkedListLayer()
+    m1.append([100, 200])
+    m1.append([1000, 2000, 3000, 4000])
+    m1.append([10000, 20000, 30000, 40000])
+    m1.append([100000, 200000, 300000, 400000])
+    m1.append([1000000, 2000000, 3000000, 4000000])
+    m2 = MultiHeadLinkedListLayer()
+    m2.append([0.1, 0.2])
+    m2.append([0.01, 0.02, 0.03, 0.04])
+    m2.append([0.001, 0.002, 0.003, 0.004, 0.005])
+    m = m + m1
+    m.append([100])
+    m = m + m2
+    m.append([11, 13, 15])
+    g = Generator(m)
+    assert g[3] == [[100, None], [1000, None], [10000, None], [100000, 1], [4000000, 10], [DummyConcat], [100]]
 
 def test_multimodal_network_graph_dump_1():
     m1 = MultiHeadLinkedListLayer()
--- tnng/generator.py
+++ tnng/generator.py
@@ -15,83 +15,83 @@ class Generator:
     ):
         self.multi_head_linked_list_layer = multi_head_linked_list_layer
         self.dump_nn_graph = dump_nn_graph
-        self._len = self._calc_num_of_all_combination(multi_head_linked_list_layer)
-        if self.dump_nn_graph:
-            self._backbone_graph, self._node_features = \
-                self._create_backbone_graph(multi_head_linked_list_layer)
+        self._len, self._node_type_layers = self._calc_num_of_all_combination(multi_head_linked_list_layer)
 
-    def _calc_num_of_all_combination(self, multi_head_linked_list_layer) -> int:
+    def _calc_num_of_all_combination(self, multi_head_linked_list_layer) -> (int, typing.List[int]):
         ''' calculate each the number of layer's combination.
         '''
         num = 1
+        node_type_layers = []
         for node in self.multi_head_linked_list_layer.graph.nodes(data=True):
             if not 'args' in node[1].keys():
                 continue
             if not node[1]['args']:
                 continue
-            _n_type = len(node[1]['args'])
+            if 'layer' in node[1].keys():
+                # append_lazy
+                _n_type = len(node[1]['args'])
+            else:
+                _n_type = len(node[1]['args'][0])
+            node_type_layers.append(_n_type)
             num *= _n_type
-        return num
+        return num, node_type_layers
 
     def __getitem__(self, idx):
         if idx > self._len:
             raise IndexError(f"access by {idx}, max length is {self._len}")
-        layer_index_list = self._get_layer_index_list(idx)
-        out = []
-        cur = [self.multi_head_linked_list_layer.tail,]
-        for layer_indcies in layer_index_list:
-            # from tail to head
-            layer = []
-            parents = []
-            for current_layer, l_idx in zip(cur, layer_indcies):
-                if current_layer is None:
-                    layer.append(None)
-                elif current_layer.layers is not None:
-                    if isinstance(current_layer, Layer):
-                        layer.append(current_layer.layers[l_idx])
-                    elif isinstance(current_layer, LazyLayer):
-                        kwargs = current_layer.kwargs_list[l_idx]
-                        klass = current_layer.klass
-                        if klass == DummyConcat:
-                            layer.append(current_layer.klass)
-                        else:
-                            layer.append(current_layer.klass(**kwargs))
-                    else:
-                        raise NotImplementedError
+        graph = self.multi_head_linked_list_layer.graph
+        layer_index_list = self._get_layer_index_list(idx, self._node_type_layers)
+        last_node = len(graph.nodes()) - 1
+        no_ordered_outputs = []
+        outputs = []
+        for layer_idx, node in zip(layer_index_list, graph.nodes(data=True)):
+            if 'layer' in node[1].keys():
+                # append_lazy
+                layer = node[1]['layer']
+                kwargs = node[1]['args'][layer_idx]
+                no_ordered_outputs.append(layer(**kwargs))
+            else:
+                value = node[1]['args'][0][layer_idx]
+                no_ordered_outputs.append(value)
+        outputs.append([no_ordered_outputs[last_node],])
+
+        parents = list(graph.predecessors(last_node))
+        while True:
+            if parents.count(None) == len(parents):
+                break
+            _output = []
+            _parents = []
+            for parent in parents:
+                if parent is None:
+                    _output.append(None)
+                    __parents = [None,]
                 else:
-                    layer.append(None)
-                if current_layer is None:
-                    parents.append(None)
-                    continue
-                elif current_layer.parent is None:
-                    parents.append(None)
-                    continue
-                for parent in current_layer.parent:
-                    parents.append(parent)
-            cur = parents
-            out.append(layer)
+                    _output.append(no_ordered_outputs[parent])
+                    __parents = list(graph.predecessors(parent))
+                if not __parents:
+                    _parents += [None,]
+                else:
+                    _parents += __parents
+            outputs.append(_output)
+            parents = _parents
         if self.dump_nn_graph:
             nodelist = range(len(self._backbone_graph.nodes()))
             adj = nx.to_numpy_matrix(self._backbone_graph, nodelist=nodelist)
             node_features = self._create_node_features(idx, layer_index_list)
-            return out[::-1], (adj, node_features)
+            return outputs[::-1], (adj, node_features)
         else:
-            return out[::-1]
+            return outputs[::-1]
 
     def _create_node_features(self, idx, layer_index_list):
         node_features = [[]]
         return np.vstack(node_features)
 
-    def _get_layer_index_list(self, idx: int):
+    def _get_layer_index_list(self, idx: int, node_type_layers):
         _idx = idx
         layer_index_list = []
-        graph = self.multi_head_linked_list_layer.graph
-        for eles in self.layer_num_list:
-            _index_list = []
-            for _, ele in enumerate(eles):
-                _index_list.append(_idx % ele)
-                _idx //= ele
-            layer_index_list.append(_index_list)
+        for n_args in node_type_layers:
+            layer_index_list.append(_idx % n_args)
+            _idx //= n_args
         return layer_index_list
 
     def __len__(self):
